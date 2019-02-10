ffc716cb7e8e8f828d3c7f0f1fded503353916b6
Forrest Jacobs <forrestjacobs@gmail.com>
2019-02-10 14:17:07 -0500

    Use correct arg in kanjidic2sql script

--- packages/kanjidic2sql/src/index.ts
+++ packages/kanjidic2sql/src/index.ts
@@ -5,8 +5,8 @@ import { createGunzip } from "zlib";
 import { KanjiDic2Parser } from "./kanjidic2parser";
 import { UpdateCharacterStream } from "./update-character-stream";
 
-const kanjiDic2Stream = (process.argv.length > 1) ?
-  createReadStream(process.argv[2], "utf8") :
+const kanjiDic2Stream = (process.argv.length > 2) ?
+  createReadStream(process.argv[3], "utf8") :
   get("http://www.edrdg.org/kanjidic/kanjidic2.xml.gz").pipe(createGunzip());
 
 kanjiDic2Stream
