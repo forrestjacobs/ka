a23c506247b66d56e2f110c5479fd3a0a1c0c12d
Forrest Jacobs <forrestjacobs@gmail.com>
2019-03-11 17:54:09 -0400

    Remove dependency on redux-devtools-extension

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,6 +1,5 @@
 import { render } from "react-dom";
-import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { applyMiddleware, compose as reduxCompose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { getApi } from "./api";
 import { Root } from "./components/root";
@@ -9,9 +8,13 @@ import { getBundles } from "./localizations";
 import { rootReducer } from "./reducers";
 
 const isProd = process.env.NODE_ENV === "production";
-const enhancer = applyMiddleware(thunk.withExtraArgument({ api: getApi() }));
+const compose: typeof reduxCompose = (!isProd && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || reduxCompose;
 
-const store = createStore(rootReducer, isProd ? enhancer : composeWithDevTools(enhancer));
+const store = createStore(
+  rootReducer,
+  compose(applyMiddleware(thunk.withExtraArgument({ api: getApi() }))),
+);
+
 const bundles = getBundles();
 
 render(Root(store, bundles), document.getElementById("root"));
