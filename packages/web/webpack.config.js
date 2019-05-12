096d8228b1b316e6dc22596c2cf2562cbf345af0
Forrest Jacobs <forrestjacobs@gmail.com>
2019-05-11 21:14:37 -0400

    Merge webpack config files

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -1,42 +1,23 @@
 const path = require("path");
+const { getIfUtils, removeEmpty } = require("webpack-config-utils");
 
-const webpack = require("webpack");
 const glob = require("glob");
+const webpack = require("webpack");
 
 const autoprefixer = require("autoprefixer");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const template = require("html-webpack-template");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const PurifyCSSPlugin = require("purifycss-webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-module.exports = function(isProd) {
-  const plugins = [
-    new HtmlWebpackPlugin({
-      template,
-      inject: false,
-      mobile: true,
-      title: "Kanji Dictionary",
-      baseHref: "/",
-      appMountId: "root"
-    }),
-    new webpack.DefinePlugin({
-      "process.env.TARGET": JSON.stringify("web"),
-      "process.env.API_URL": JSON.stringify(
-        process.env.API_URL || "http://localhost:3000"
-      )
-    })
-  ];
-
-  if (isProd) {
-    plugins.push(
-      new MiniCssExtractPlugin(),
-      new PurifyCSSPlugin({
-        paths: glob.sync(path.join(__dirname, "src/**/*.{ts,tsx}"))
-      })
-    );
-  }
+module.exports = function(env) {
+  const mode = env && env.production ? "production" : "development";
+  const { ifProduction, ifDevelopment } = getIfUtils(mode);
 
   return {
+    mode,
     entry: {
       main: "./src/index.ts",
       styles: "./styles/index.scss"
@@ -54,7 +35,7 @@ module.exports = function(isProd) {
         {
           test: /\.scss$/,
           use: [
-            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            ifProduction(MiniCssExtractPlugin.loader, "style-loader"),
             "css-loader",
             {
               loader: "postcss-loader",
@@ -69,7 +50,7 @@ module.exports = function(isProd) {
           type: "javascript/auto",
           loader: "messageformat-loader",
           options: {
-            disablePluralKeyChecks: isProd
+            disablePluralKeyChecks: ifProduction()
           },
           include: path.resolve(__dirname, "src")
         }
@@ -85,10 +66,44 @@ module.exports = function(isProd) {
         "@ka/base": path.resolve(__dirname, "../base/src/index.ts")
       }
     },
+    output: ifProduction({
+      path: path.resolve(__dirname, "dist")
+    }),
+    devtool: ifDevelopment("inline-source-map"),
+    devServer: ifDevelopment({
+      host: "0.0.0.0",
+      port: 8080,
+      historyApiFallback: true
+    }),
     optimization: {
-      splitChunks: { chunks: "all" }
+      splitChunks: { chunks: "all" },
+      minimizer: ifProduction([
+        new TerserPlugin(),
+        new OptimizeCSSAssetsPlugin()
+      ])
     },
     watchOptions: { poll: true },
-    plugins
+    plugins: removeEmpty([
+      new HtmlWebpackPlugin({
+        template,
+        inject: false,
+        mobile: true,
+        title: "Kanji Dictionary",
+        baseHref: "/",
+        appMountId: "root"
+      }),
+      new webpack.DefinePlugin({
+        "process.env.TARGET": JSON.stringify("web"),
+        "process.env.API_URL": JSON.stringify(
+          process.env.API_URL || "http://localhost:3000"
+        )
+      }),
+      ifProduction(new MiniCssExtractPlugin()),
+      ifProduction(
+        PurifyCSSPlugin({
+          paths: glob.sync(path.join(__dirname, "src/**/*.{ts,tsx}"))
+        })
+      )
+    ])
   };
 };
